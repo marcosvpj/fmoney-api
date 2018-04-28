6ac24d7c0d24a4d342a685ed2c44338c57771a3f
Marcosvpj <marcos@marcosvpj.com.br>
2018-04-27 21:14:32 -0300

    model transaction

--- src/fmoney/core/admin.py
+++ src/fmoney/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from fmoney.core.models import Transaction
 # Register your models here.
+
+
+class TransactionModelAdmin(admin.ModelAdmin):
+    list_display = ['due_date', 'title', 'value']
+
+admin.site.register(Transaction, TransactionModelAdmin)
--- src/fmoney/core/models.py
+++ src/fmoney/core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Transaction(models.Model):
+    title = models.CharField('titulo', max_length=255)
+    value = models.DecimalField('valor', max_digits=10, decimal_places=2)
+    due_date = models.DateField('data de vencimento')
+    payment_method = models.CharField('metodo de pagamento', max_length=255, blank=True)
+    place = models.CharField('local', max_length=255, blank=True, null=True, default=None)
+    paid = models.BooleanField('pago', default=False)
+
+    class Meta:
+        verbose_name = 'transação'
+        verbose_name_plural = 'transações'
+
+    def __str__(self):
+        return self.title
