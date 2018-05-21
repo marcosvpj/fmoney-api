ce701c1ee382211eb4af07a5d69100f98a9bf4d8
kallel anobom <kallel@paladiotecnologia.com.br>
2018-05-20 21:33:50 -0300

    Editando o formato do campo data padrao PT e alterando language code do python

--- src/fmoney/settings.py
+++ src/fmoney/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -114,7 +114,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
